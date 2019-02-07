e64c1e3f4d2c209139cd2ba7b76eae3508b401ef
Maurizio Terreni <maurizio.terreni@edpser.eu>
2019-02-07 17:48:14 +0100

    creato layout principale

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/dashboard'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from './app.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { HttpLoaderFactory } from './factory/httpLoaderFactory';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { TopnavbarComponent } from './layout/template/topnavbar/topnavbar.component';
+import { AsidenavbarComponent } from './layout/template/asidenavbar/asidenavbar.component';
+import { FooternavbarComponent } from './layout/template/footernavbar/footernavbar.component';
+import { SettingsnavbarComponent } from './layout/template/settingsnavbar/settingsnavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TopnavbarComponent,
+    AsidenavbarComponent,
+    FooternavbarComponent,
+    SettingsnavbarComponent,
   ],
   imports: [
     BrowserModule,
